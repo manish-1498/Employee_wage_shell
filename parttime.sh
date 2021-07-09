f94c0b30b4842f3283f443c650f6809fd1fc678a
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-09 16:00:45 +0530

    added:parttime

---
+++ parttime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+x=$((RANDOM%2))
+dailyperhr=8
+rateperday=20
+parttimehr=4
+if [ $x -eq 0 ]
+then
+echo employee is absent
+dailywage=0
+else
+echo employee is present
+dailywage=$(($dailyperhr*$rateperday))
+fi
